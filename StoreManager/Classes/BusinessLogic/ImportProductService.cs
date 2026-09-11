25468650b4b4198445e60e9d11c0b302db269028
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-11 22:27:40 +0300

    Implement ImportProductService business logic layer

--- StoreManager/Classes/BusinessLogic/ImportProductService.cs
+++ StoreManager/Classes/BusinessLogic/ImportProductService.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using StoreManager.Database;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StoreManager.Classes.BusinessLogic
 {
     internal class ImportProductService
     {
+        private StoreManager.Database.Repositories.ImportProductRepository _Repository;
+
+        public ImportProductService()
+        {
+            _Repository = new StoreManager.Database.Repositories.ImportProductRepository();
+        }
+
+        public List<CategoriesTabel> GetAllCategories()
+        {
+            return _Repository.GetAllCategories();
+        }
+
+        public SuppliersTabel GetSupplierByPhoneOrEmail(string search)
+        {
+            return _Repository.GetSupplierByPhoneOrEmail(search);
+        }
+
+        public bool ProductCodeExists(string code)
+        {
+            return _Repository.ProductCodeExists(code);
+        }
+
+        public void AddProduct(ProductsTabel product)
+        {
+            _Repository.AddProduct(product);
+        }
+
+        public void UpdateProductImage(ProductsTabel product, string imagePath)
+        {
+            _Repository.UpdateProductImage(product, imagePath);
+        }
+
+        public ProductsTabel GetProductById(int id)
+        {
+            return _Repository.GetProductById(id);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            _Repository.DeleteProduct(id);
+        }
+
+        public bool CategoryExists(string categoryName)
+        {
+            return _Repository.CategoryExists(categoryName);
+        }
+
+        public void AddCategory(CategoriesTabel category)
+        {
+            _Repository.AddCategory(category);
+        }
+
+        public void AddPurchaseBill(PurchaseBillTabel bill)
+        {
+            _Repository.AddPurchaseBill(bill);
+        }
+
+        public void AddPurchaseBillDetail(PurchaseBillDetailsTabel detail)
+        {
+            _Repository.AddPurchaseBillDetail(detail);
+        }
+
+        public void SaveChanges()
+        {
+            _Repository.SaveChanges();
+        }
+
+        public void DeleteProducts(List<int> ids)
+        {
+            _Repository.DeleteProducts(ids);
+        }
     }
 }
